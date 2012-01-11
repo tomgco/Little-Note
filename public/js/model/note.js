e34fcdef59a75a7f93b9357b8ccd7826cc7f927b
Tom Gallacher <tomgallacher23@gmail.com>
2012-01-11 01:01:08 +0000

    Initial MVC setup

--- public/js/model/note.js
+++ public/js/model/note.js
@@ -25,15 +25,137 @@ window.NoteList = Backbone.Collection.extend({
 window.Notes = new NoteList;
 
 window.NoteView = Backbone.View.extend({
+
 	tagName: 'li',
 
+	// Cache the template function for a single note.
+	template: _.template($('#item-template').html()),
+
 	events: {
-		'#btn_save': 'saveNote',
-		'#btn_del': 'deleteNote'
+		'click .save': 'saveNote',
+		'click .delete': 'deleteNote',
+		'click .preview': 'previewNote'
 	},
 
 	initialize: function() {
 		this.model.bind('change', this.render, this);
 		this.model.bind('destroy', this.remove, this);
+	},
+
+	// Re-render the contents of the note.
+	render: function() {
+		$(this.el).html(this.template(this.model.toJSON()));
+		this.setTitle();
+		this.setDate();
+		this.setBody();
+		return this;
+	},
+
+	setTitle: function() {
+		var title = this.model.get('title');
+		this.date = this.$('.title').text(title);
+	},
+
+	setDate: function() {
+		var date = this.model.get('date-modified');
+		this.$('.date-modified').text(date);
+	},
+
+	setBody: function() {
+		var body = this.model.get('body');
+		this.textarea = this.$('.note-body').val(body);
+	},
+
+	close: function() {
+		this.model.save({
+			body: this.textarea.val(),
+			title: this.date.text()
+		});
+	},
+
+	// Remove this view from the DOM.
+	remove: function() {
+		$(this.el).remove();
+	},
+
+	// Remove the item, destroy the model.
+	clear: function() {
+		this.model.destroy();
 	}
+
 });
+
+window.NoteListView = Backbone.View.extend({
+
+	tagName: 'li',
+
+	template: _.template($('#list-item-template').html()),
+
+	events: {
+		'click .load': 'loadNote'
+	},
+
+	initialize: function() {
+		this.model.bind('change', this.render, this);
+		this.model.bind('destroy', this.remove, this);
+	},
+
+	// Re-render the contents of the note.
+	render: function() {
+		$(this.el).html(this.template(this.model.toJSON()));
+		this.setTitle();
+		return this;
+	},
+
+	setTitle: function() {
+		var title = this.model.get('title');
+		this.date = this.$('.title').text(title);
+	},
+
+	// Remove this view from the DOM.
+	remove: function() {
+		$(this.el).remove();
+	},
+
+	// Remove the item, destroy the model.
+	clear: function() {
+		this.model.destroy();
+	}
+
+});
+
+window.AppView = Backbone.View.extend({
+
+	// Instead of generating a new element, bind to the existing skeleton of
+	// the App already present in the HTML.
+	el: $("#little-note-app"),
+
+	// At initialization we bind to the relevant events on the `Todos`
+	// collection, when items are added or changed. Kick things off by
+	// loading any preexisting todos that might be saved in *localStorage*.
+	initialize: function() {
+		this.input = this.$("#new-note");
+
+		Notes.bind('add',   this.addOne, this);
+		Notes.bind('reset', this.addAll, this);
+		Notes.bind('all',   this.render, this);
+
+		Notes.fetch();
+	},
+
+	// Add a single todo item to the list by creating a view for it, and
+	// appending its element to the `<ul>`.
+	addOne: function(todo) {
+		var view = new NoteListView({model: note});
+		$("#note-list").append(view.render().el);
+	},
+
+	// Add all items in the **Todos** collection at once.
+	addAll: function() {
+		Notes.each(this.addOne);
+	}
+
+});
+
+// Finally, we kick things off by creating the **App**.
+window.App = new AppView;
